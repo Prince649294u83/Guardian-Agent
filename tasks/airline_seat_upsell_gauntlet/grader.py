94a8804e5cbd819f0d399923f58a5f476a3ed669
Prince <pjrathnakar@gmail.com>
2026-04-12 13:10:48 +0530

    Maximize deep-validator grader compatibility

--- tasks/airline_seat_upsell_gauntlet/grader.py
+++ tasks/airline_seat_upsell_gauntlet/grader.py
@@ -1,9 +1,9 @@
 from typing import Any
 from guardian_openenv.task_graders import grade_airline_seat_upsell_gauntlet
 
-def grade(*args: Any, **kwargs: Any) -> float:
+def grade(*args: Any, **kwargs: Any):
     """Grader function mapped for the OpenEnv Phase 2 validator."""
     try:
-        return float(grade_airline_seat_upsell_gauntlet(*args, **kwargs))
+        return grade_airline_seat_upsell_gauntlet(*args, **kwargs)
     except Exception:
         return 0.001
